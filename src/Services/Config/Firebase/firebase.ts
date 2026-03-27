e0449b23feaf24f0e6e017408e87f366211c4390
Himanshu Saha <himanshusaha@Himanshus-MacBook-Air.local>
2026-03-27 12:28:47 +0530

    chore(firebase): changed Firebase config

--- src/Services/Config/Firebase/firebase.ts
+++ src/Services/Config/Firebase/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBLsW2dvix5E3jEVCrfzhOI8_XzmjRzHHE",
-  authDomain: "notes-c8927.firebaseapp.com",
-  projectId: "notes-c8927",
-  storageBucket: "notes-c8927.firebasestorage.app",
-  messagingSenderId: "219581579497",
-  appId: "1:219581579497:web:12a2978b210351c52e7078",
-  measurementId: "G-J66EM0F669"
+  apiKey: 'AIzaSyBLsW2dvix5E3jEVCrfzhOI8_XzmjRzHHE',
+  authDomain: 'notes-c8927.firebaseapp.com',
+  projectId: 'notes-c8927',
+  storageBucket: 'notes-c8927.firebasestorage.app',
+  messagingSenderId: '219581579497',
+  appId: '1:219581579497:web:12a2978b210351c52e7078',
+  measurementId: 'G-J66EM0F669',
 };
 
 // Initialize Firebase
